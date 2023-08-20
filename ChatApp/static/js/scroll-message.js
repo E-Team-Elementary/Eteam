f331662e31f9409cac4126094655bd1b6499147c
oonatsu <nabari008@gmail.com>
2023-08-20 22:41:45 +0900

    search.jsに検索後、元に戻せるように変更

--- ChatApp/static/js/scroll-message.js
+++ ChatApp/static/js/scroll-message.js
@@ -4,4 +4,7 @@ window.onload = function () {
 
   const scroller2 = document.getElementById("note-area");
   scroller2.scrollTop = scroller2.scrollHeight - scroller2.clientHeight;
+
+  const search_delete = document.getElementById("delete_search");
+  search_delete.style.visibility = "hidden";
 };
