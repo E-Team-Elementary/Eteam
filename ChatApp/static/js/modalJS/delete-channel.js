f27a194d5c915c0cdb4a6fa0c64d801151e92397
宇田津勇太 <utatsuyuuta@udatsu.local>
2023-08-22 00:52:04 +0900

    public削除モーダル表示

--- ChatApp/static/js/modalJS/delete-channel.js
+++ ChatApp/static/js/modalJS/delete-channel.js
@@ -1,16 +1,21 @@
 const publicDeleteModal = document.getElementById("delete-public-modal");
 const publicDeleteBtn = document.getElementById("trash-channel");
 
-if (publicDeleteModal) {
+const deleteForm = document.getElementById("public-channel-delete");
 
-  // When the user clicks the button, open the modal.
-    publicDeleteBtn.onclick = function() {
-    publicDeleteModal.style.display = 'flex';
-  };
-
-  addEventListener("click", (e) => {
-    if (e.target == publicDeleteModal) {
-    publicDeleteModal.style.display = "none";
-    }
-  });
+function deleteChannel(channel_id) {
+  publicDeleteModal.style.display = "flex";
+  const inputHiddenElement = document.createElement("input");
+  inputHiddenElement.type = "hidden";
+  inputHiddenElement.id = "public_delete_channel_id";
+  inputHiddenElement.value = channel_id;
+  deleteForm.insertBefore(inputHiddenElement, deleteForm.firstChild);
 }
+
+addEventListener("click", (e) => {
+  if (e.target == publicDeleteModal) {
+    publicDeleteModal.style.display = "none";
+    const deleteElement = document.getElementById("public_delete_channel_id");
+    deleteForm.removeChild(deleteElement);
+  }
+});
