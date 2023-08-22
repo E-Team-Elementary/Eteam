f354c1576b87c3e61048d4917df30f25e70c6e04
宇田津勇太 <utatsuyuuta@udatsu.local>
2023-08-22 23:02:37 +0900

    public編集ボタン修正

--- ChatApp/util/DB.py
+++ ChatApp/util/DB.py
@@ -5,14 +5,17 @@ class DB:
     def getConnection():
         try:
             connection = pymysql.connect(
-                host="chatapp.ci06votjyvzz.ap-northeast-1.rds.amazonaws.com",
+                # host="chatapp.ci06votjyvzz.ap-northeast-1.rds.amazonaws.com",
+                host="db",
                 db="chatapp",
-                user="admin",
-                password="hJpQ9B4A",
+                # user="admin",
+                # password="hJpQ9B4A",
+                user="testuser",
+                password="testuser",
                 charset="utf8",
-                cursorclass=pymysql.cursors.DictCursor
+                cursorclass=pymysql.cursors.DictCursor,
             )
             return connection
-        except (ConnectionError):
+        except ConnectionError:
             print("コネクションエラーです")
             connection.close()
