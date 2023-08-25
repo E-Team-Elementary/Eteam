df3fa0a991bbc9dc8db9642da5b0ff14be971d8c
宇田津勇太 <utatsuyuuta@udatsu.local>
2023-08-26 02:33:30 +0900

    アラートメッセージ修正

--- ChatApp/static/js/flaskMessage.js
+++ ChatApp/static/js/flaskMessage.js
@@ -1,8 +1,20 @@
+// flashメッセージがある時に、アラート起動する
+
 const flashMessage = document.getElementById("flashMessage");
 
 if (flashMessage) {
-  Swal.fire({
-    icon: "success",
-    text: flashMessage.value,
-  });
+  const splitFlashMessage = flashMessage.value.split(":");
+
+  // NG系のアラートはアイコンをバツマークで表示させる
+  if ("NG" == splitFlashMessage[0]) {
+    Swal.fire({
+      icon: "error",
+      text: splitFlashMessage[1],
+    });
+  } else {
+    Swal.fire({
+      icon: "success",
+      text: splitFlashMessage[1],
+    });
+  }
 }
